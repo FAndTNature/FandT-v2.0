44eac91be07a399939ae61b024851e561094f2f0
FAndTNature <80643701+FAndTNature@users.noreply.github.com>
2021-03-15 21:56:58 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,7 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_ID))
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile('/app/frontend/build/index.html')
     })
 }
 else {
